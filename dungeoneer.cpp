b89c45e0270146bc2a15239f1c8cd839dc3a7713
FireShredder24 <200917875@student.lbschools.net>
2018-08-15 14:24:40 -0700

    Random event API
    Created a function to generate a random event and implemented loot for non-food enemies like goblins and thieves, but not police

--- dungeoneer.cpp
+++ dungeoneer.cpp
@@ -65,7 +65,7 @@ string tempstrg;
 // Predefining NPC "Enemy" (which is redefined and reused throughout Dungeoneer) to prevent compile errors
 // Notice that this generic NPC is a flat character--its ability scores are all the same!
 // An actual player would never create a character like this.(but its a good representation of a generic 1st level unclassed enemy)(and might be hilarious for a 1st-level wizard...)
-NPC Enemy ("Enemy", 13, 13, 13, 13, 13, 13, 6, 8, 4, 12, true, -1, 1, "scimitar", "light crossbow", "dagger", 2);
+NPC Enemy ("goblin", 13, 13, 13, 13, 13, 13, 6, 8, 4, 12, true, -1, 1, "scimitar", "light crossbow", "dagger", 2);
 void setInvNames () { // Setting the array of names for the items in Dungeoneer.
 invNames[1] = "longsword";
 invNames[2] = "shortsword";
@@ -134,9 +134,9 @@ void NPC::dealDamage (int atkType, bool hasAdv, bool hasDis) { // Implement meth
 		}
 		if (damage >= 1) {
 		HP = HP - damage;
-		cout << "Your enemy attacks with his " << tempstrg << ", dealing " << damage << " damage to you.  You are now at " << HP << " HP." << endl;
+		cout << "The " << Name << " attacks with his " << tempstrg << ", dealing " << damage << " damage.  You are now at " << HP << " HP." << endl;
 		} else {
-			cout << "Your armor deflects your enemy's " << tempstrg << ", and you are unharmed." << endl;
+			cout << "Your armor deflects the " << Name  << "'s " << tempstrg << ", and you are unharmed." << endl;
 		}
 	} else if (atkRoll >= 10 && !isEnemy && !isDead && !isIncap) {
 		damage = toprand(damDie[atkType]);
@@ -144,7 +144,7 @@ void NPC::dealDamage (int atkType, bool hasAdv, bool hasDis) { // Implement meth
 		damage = damage + getModifier(strg); // Adding strength modifier
 		Enemy.takeDamage (damage);
 	} else if (atkRoll <= 9) 
-		cout << "Your enemy swings at you with his " << tempstrg << ", but the blow does not connect." << endl;
+		cout << "The " << Name << " attacks with their " << tempstrg << ", but the blow does not connect." << endl;
 }
 // Subtracts a specified number of hitpoints from the host NPC and incapacitates it/kills it depending upon the amount of damage taken
 void NPC::takeDamage (int damage) {
@@ -153,25 +153,38 @@ void NPC::takeDamage (int damage) {
 	if (npcHP <= 0) { // Dealing with what the damage taken does to the NPC
 		if (abs(npcHP) >= maxHP) {// If the opposite of the NPC's hitpoint maximum is greater than the current number of hitpoints, kill the NPC
 			isDead = true;
-			if (Name == "wild_boar") {
+			if (Name == "wild boar") {
 				inv[20] = inv[20] + toprand(15) + 20;
-			} else if (Name == "wild_dog") {
+			} else if (Name == "wild dog") {
 				inv[20] = inv[20] + toprand(5) + 5;
-			} else if (Name == "wild_turkey") {
+			} else if (Name == "wild turkey") {
 				inv[20] = inv[20] + toprand(20) + 10;
-			} else if (Name == "wild_bull") {
+			} else if (Name == "wild bull") {
 				inv[20] = inv[20] + toprand(20) + 40;
-			} else if (Name == "wild_badger") {
+			} else if (Name == "wild badger") {
 				inv[20] = inv[20] + toprand(15) + 5;
 			} else if (Name == "Phil Swift") {
 				inv[29] = inv[29] + 100;
 				cout << "You pick up 100 FlexTape from Phil Swift's earthly body as his soul returns to heaven.  You hear his voice telling you that FlexTape can be used to treat wounds instantly.\n";
+			} else if (Name == "goblin") {
+				inv[9]+=1;
+				inv[24]+=1;
+				inv[27]+=25;
+				inv[4]+=1;
+				inv[5]+=1;
+				inv[10]+=20;
+			} else if (Name == "thief") {
+				inv[5]+=3;
+				inv[27]+=10;
+				inv[26]+=1;
+				inv[17]+=1;
+				inv[4]+=1;
 			}
 		}
 		else {
 		isIncap = true; // If not, render it unconcious
 		//cout << "Kim Jong-Un attacks, dealing 9001 Communist Damage!" << endl;
-		cout << "Your enemy faints from the pain of your attack and is now unconcious! ";
+		cout << "The " << Name << " falls unconscious. ";
 		}
 	} // This system is not completely accurate to D&D, as in the game most DMs have enemies die when they drop to 0 HP.  Also, this stores HP as a negative value--something not done in D&D.
 }
@@ -249,7 +262,7 @@ void attack (int atkType, bool hasAdv, bool hasDis) {
 		cout << "You successfully damaged your target, dealing " << damage << " damage.  Your target is now at " << Enemy.npcHP << " HP. " << endl;
 		if (Enemy.isDead == true) {
 			cout << "You killed your enemy." << endl;
-			Enemy.set ("Enemy", 13, 13, 13, 13, 13, 13, 6, 8, 4, 12, true, -1, 1, "scimitar", "light crossbow", "dagger", 2);
+			Enemy.set ("goblin", 13, 13, 13, 13, 13, 13, 6, 8, 4, 12, true, -1, 1, "scimitar", "light crossbow", "dagger", 2);
 			inCombat = false;
 			act ();
 		} else {
@@ -265,6 +278,91 @@ void attack (int atkType, bool hasAdv, bool hasDis) {
 		cout << "You failed to damage your target. " << endl;
 		Enemy.dealDamage (toprand(3), false, false);
 		act();
+	}
+}
+void randomEvent () {
+	int tempint = toprand(3);
+	if (tempint == 1) {
+		tempint = toprand(4);
+		switch (tempint) {
+			case 1:
+				Enemy.set ("goblin", 13, 13, 13, 13, 13, 13, 6, 8, 4, 12, true, -1, 1, "scimitar", "light crossbow", "dagger", 2);
+				cout << "You encounter a lone goblin on patrol.  You have the first move.\n";
+				inCombat = true;
+				act ();
+				break;
+			case 2:
+				Enemy.set ("dire wolf", 16, 13, 18, 13, 18, 16, 8, 6, 4, 10, true, 2, 1, "teeth", "claws", "paws", 1);
+				cout << "A dire wolf sneaks up on you from behind and pounces on your back.\n";
+				Enemy.dealDamage (1, true, false);
+				inCombat = true;
+				act ();
+				break;
+			case 3:
+				Enemy.set ("thief", 11, 11, 15, 15, 8, 8, 4, 8, 4, 12, true, -1, 1, "fists", "crowbar", "dagger", 1);
+				cout << "A thief punches you in the guts as he pulls out a dagger, demanding you hand over your money.\n";
+				Enemy.dealDamage (1, true, false);
+				inCombat = true;
+				act ();
+				break;
+			case 4:
+				Enemy.set ("rat", 8, 11, 16, 1, 8, 1, 4, 6, 4, 6, true, -1, 1, "claws", "teeth", "claws", 0);
+				cout << "A mangy rat leaps up and bites your leg.\n";
+				Enemy.dealDamage(2, false, false);
+				inCombat = true;
+				act ();
+				break;
+		}
+	} else if (tempint == 2) {
+		tempint = toprand(6);
+		switch (tempint) {
+			case 1:
+				cout << "You found an empty canteen with a hole in it.  +1 Metal\n";
+				inv[22] = inv[22] + 1;
+				break;
+			case 2:
+				cout << "You found a rusted longsword lying beside a suit of armor.  +3 Metal +1 Light Armor\n";
+				inv[22] = inv[22] + 3;
+				inv[23] = inv[23] + 1;
+				break;
+			case 3:
+				cout << "A bird dropped a pair of pants on your head.  +1 Clothes\n";
+				inv[26] += 1;
+				break;
+			case 4:
+				cout << "An old backpack was given to you by some random peasant who is probably insane.  +1 Backpack\n";
+				inv[28] += 1;
+				break;
+			case 5:
+				cout << "A stack of firewood catches your attention.  +5 Firewood\n";
+				inv[16] += 5;
+				break;
+			case 6:
+				cout << "An apple fell out of a tree nearby.  +1 Food\n";
+				inv[20] += 1;
+				break;
+		}
+	} else if (tempint == 3) {
+		tempint = toprand(6);
+		switch (tempint) {
+			case 1:
+				cout << "An old man comes up to you and starts babbling about his stupid grandson.\n"; break;
+			case 2:
+				cout << "A peasant boy throws a rock at the back of your head. -1 HP\n";
+				HP -= 1; break;
+			case 3:
+				cout << "A benevolent spirit visits you and tells you you're a hero.\n";
+				break;
+			case 4:
+				cout << "A fish says hi.\n";
+				break;
+			case 5:
+				cout << "You see a tree that appears to be growing lanterns on its branches.\n";
+				break;
+			case 6:
+				cout << "You spot a cloak on the side of the road, but you pass it by.\n";
+				break;
+		}
 	}
 }
 void mover () {
@@ -307,6 +405,12 @@ void mover () {
 				inv[20] = inv[20] - distint;
 				x = newx;
 				y = newy;
+				int i = 1;
+				do {
+					cout << "On day " << i << " ...\n";
+					i = i + 1;
+					randomEvent();
+				} while (i <= distint);
 				cout << "You have traveled to " << place << " over the course of " << distint << " days.  You have " << inv[20] << " days of rations left.\n";
 			} else if (query == "no" || query == "n") {
 				cout << "You decide to stay where you are.\n";
@@ -526,27 +630,27 @@ void act () {
 					case 10:
 					case 11:
 					case 12:
-						Enemy.set ("wild_boar", 14, 14, 10, 9, 9, 9, 6, 8, 4, 8, true, -2, 1, "claws", "teeth", "paws", 1);
+						Enemy.set ("wild boar", 14, 14, 10, 9, 9, 9, 6, 8, 4, 8, true, -2, 1, "claws", "teeth", "paws", 1);
 						cout << "You encounter a wild boar. ";
 						break;
 					case 13:
 					case 14:
 					case 15:
-						Enemy.set ("wild_dog", 14, 14, 10, 9, 9, 9, 6, 6, 4, 6, true, -3, 1, "claws", "jaws", "dog doo", 1);
+						Enemy.set ("wild dog", 14, 14, 10, 9, 9, 9, 6, 6, 4, 6, true, -3, 1, "claws", "jaws", "dog doo", 1);
 						cout << "You encounter a wild dog.";
 						break;
 					case 16:
-						Enemy.set ("wild_turkey", 14, 14, 10, 9, 9, 9, 0, 0, 0, 6, true, -4, 1, "feathers", "gobble", "tail", 1);
+						Enemy.set ("wild turkey", 14, 14, 10, 9, 9, 9, 0, 0, 0, 6, true, -4, 1, "feathers", "gobble", "tail", 1);
 						cout << "You encounter a wild turkey.";
 						break;
 					case 17:
-						Enemy.set ("wild_bull", 16, 14, 10, 9, 9, 9, 8, 8, 10, 12, true, -5, 1, "hoof", "head", "horns", 1);
+						Enemy.set ("wild bull", 16, 14, 10, 9, 9, 9, 8, 8, 10, 12, true, -5, 1, "hoof", "head", "horns", 1);
 						cout << "You encounter a bull.";
 						break;
 					case 18:
 					case 19:
 					case 20:
-						Enemy.set ("wild_badger", 14, 14, 10, 9, 9, 9, 8, 6, 10, 10, true, -6, 1, "claws", "paws", "jaws", 1);
+						Enemy.set ("wild badger", 14, 14, 10, 9, 9, 9, 8, 6, 10, 10, true, -6, 1, "claws", "paws", "jaws", 1);
 						cout << "You encounter a wild honey badger.";
 						break;
 				}
